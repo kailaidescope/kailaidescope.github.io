941710e5eaf1870b628e3d9915f87f2c5b9707cd
kai turanski <kai.turanski@mail.mcgill.ca>
2024-04-10 17:39:43 -0400

    Figured out coloring issue

--- aulka/world-map/assets/js/main.js
+++ aulka/world-map/assets/js/main.js
@@ -1,17 +1,22 @@
-var a = document.getElementById("mapsvg");
-var drawn_map = document.getElementById("drawn-map");
+var map_html = document.getElementById("map-svg");
+var button = document.getElementById("map-button");
+var map_svg;
+var element;
 
 // It's important to add an load event listener to the object,
 // as it will load the svg doc asynchronously
-a.addEventListener("load", function(){
+map_html.addEventListener("load", () => {
 
-	alert("hi");
-	console.log(document.getElementById("Muonir"));
+	//alert("SVG Loaded");
+	console.log(map_html.contentDocument);
+	map_svg = map_html.contentDocument;
+	element = map_svg.getElementById("Muonir");
+	console.log(element);
+
 
 	// add behaviour
-	drawn_map.addEventListener("mousedown", function(event){
-			alert('hello world!');
-			console.log(drawn_map.innerHTML);
-			document.getElementById('Muonir').setAttribute('color',"red");
+	button.addEventListener("click", (event) => {
+			//alert("Color changed");
+			element.style.fill = "blue";
 	}, false);
 }, false);
