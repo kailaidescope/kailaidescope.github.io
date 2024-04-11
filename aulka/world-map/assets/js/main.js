c371bcf3475430708c436c3743e72b4cc8ff03f1
kai turanski <kai.turanski@mail.mcgill.ca>
2024-04-11 01:18:19 -0400

    Made text work and hovering work

--- aulka/world-map/assets/js/main.js
+++ aulka/world-map/assets/js/main.js
@@ -1,7 +1,10 @@
-var map_html = document.getElementById("map-svg");
-var button = document.getElementById("map-button");
-var map_svg;
-var element;
+let map_html = document.getElementById("map-svg");
+let info_title = document.getElementById("info-title");
+let info_description = document.getElementById("info-description");
+let map_svg;
+let muonir_path;
+
+info_title.textContent = "Hover on region to learn more!";
 
 // It's important to add an load event listener to the object,
 // as it will load the svg doc asynchronously
@@ -10,13 +13,29 @@ map_html.addEventListener("load", () => {
 	//alert("SVG Loaded");
 	console.log(map_html.contentDocument);
 	map_svg = map_html.contentDocument;
-	element = map_svg.getElementById("Muonir");
-	console.log(element);
 
+	muonir_path = map_svg.getElementById("Muonir");
 
-	// add behaviour
-	button.addEventListener("click", (event) => {
-			//alert("Color changed");
-			element.style.fill = "blue";
-	}, false);
+	const regions = map_svg.getElementsByClassName("region");
+	let regions_fill = [];
+
+	for (let i = 0; i < regions.length; i++) {
+
+		regions_fill.push(regions[i].style.fill);
+
+		regions[i].addEventListener("mouseenter", () => {
+			regions[i].style.fill = "#abb823";
+			info_title.textContent = regions[i].getAttribute("inkscape:label");
+			info_description.textContent = "Dungeons & Dragons (commonly abbreviated as D&D or DnD)[2] is a fantasy tabletop role-playing game (RPG) originally created and designed by Gary Gygax and Dave Arneson.[3][4][5] The game was first published in 1974 by Tactical Studies Rules, Inc. (TSR).[5] It has been published by Wizards of the Coast, later a subsidiary of Hasbro, since 1997.";
+		}, false);
+
+		regions[i].addEventListener("mouseleave", () => {
+			regions[i].style.fill = regions_fill[i]; 
+			info_title.textContent = "Hover on region to learn more!";
+			info_description.textContent = "";
+		}, false);
+	}
+
+	console.log(regions_fill);
+	
 }, false);
