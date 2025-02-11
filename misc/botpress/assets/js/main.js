07de8c18e6bb03e1c4c97f07000d2f46422d8f5a
m3lodicaman <kai.turanski@mail.mcgill.ca>
2025-02-11 14:36:01 -0500

    Added chat and fixed styling

--- misc/botpress/assets/js/main.js
+++ misc/botpress/assets/js/main.js
@@ -1,6 +1,40 @@
-const content = document.querySelector('#article-super-container');
+const articleSuperContainer = document.querySelector('#article-super-container');
 fetch('./assets/html/navbar.html')
     .then(response => response.text())
-    .then(data => {
-        content.insertAdjacentHTML('afterbegin', data);
+    .then(navBarText => {
+        articleSuperContainer.insertAdjacentHTML('afterbegin', navBarText);
+        const navBar = document.querySelector('.sidebar');
+
+        const defaultArticleButton = navBar.querySelector(".nav-button");
+        
+        setArticle(defaultArticleButton);
     });
+
+
+
+function clickNav(button) {
+    setArticle(button);
+}
+
+function setArticle(articleButton) {
+    let articleHref = '';
+    if (articleButton.hasAttribute('href')) {
+        articleHref = articleButton.getAttribute('href');
+    }
+    
+    const contentContainer = document.querySelector('.content');
+    if (articleHref == '') {
+        contentContainer.innerHTML = '<h1>Error</h1><p>Article not found</p>';
+        return;
+    } else {
+        fetch(articleHref).then(response => {
+            if (response.status == 200) {
+                return response.text()
+            } else {
+                return '<h1>Error</h1><p>Article not found</p>';
+            }
+        }).then(articleText => {
+            contentContainer.innerHTML = articleText;
+        });
+    }
+}
